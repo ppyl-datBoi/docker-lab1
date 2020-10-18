e21fbc62ead2d6752717651e7d04a5b1f728cad6
Pavel Danilchenko <pavel.danilchenko96@gmail.com>
2020-10-18 20:22:38 +0500

    added counter

--- web-server/server.js
+++ web-server/server.js
@@ -5,12 +5,13 @@ const express = require('express');
 // константы
 const port = 8080;
 const host = '0.0.0.0';
-
+let counter = 0;
 // приложение
 const app = express();
 app.get('/', (req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(`<h1>${process.env.MESSAGE}</h1>`);
+  counter += 1;
+  res.end(`<h1>${process.env.MESSAGE} visit cointer is ${counter}</h1>`);
 });
 
 app.listen(port, host);
